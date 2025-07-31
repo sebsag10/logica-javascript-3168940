5586a20fd2a6a49067bfa1004d9ce228721bf687
Sebas <sebsag10@gmail.com>
2025-07-31 12:26:00 -0500

    repositorio js versión 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,31 @@
+// el alert pone un ventana emergente en el navegador
+//alert("Hola Mundo");
+("mundo");
+
+// muestra una caja para ingresar informaciòn desde el navegador
+//prompt("Dime Tu Nombre");
+
+//variable declarada con let
+let personaje = "Max";
+
+// crear variable puntaje y asignarle valor de cero(los valores numericos van sin comillas)
+let puntaje = 0;
+
+// crear variable shots y asignar valor de 3 (los valores numericos van sin comillas)
+let shots = 3;
+  recarga = 2;
+
+console.log("Total balas", shots + recarga);
+
+//el usuario capturo 4 patos
+puntaje += 4
+//el usuario gasto 2 balas
+shots -= 2
+//el usuario recargo su arma
+recarga += 1
+recarga ++ //++ permite sumar de a uno
+recarga ++ //++ permite sumar de a uno
+
+console.log("puntaje",puntaje)//4
+console.log("shots",shots)//1
+console.log("recarga",recarga)//3
---
+++ 2_operadores_comparacion_logicos/app.js
@@ -0,0 +1,32 @@
+// //Boolean(..)
+
+// console.log( "5" == "5" , "5" == "5" )         //true
+// console.log( "5" === "5" , "5" === "5" )       //true
+// console.log( 5 === "5" , 5 === "5" )           //false
+// console.log( 5 == "5" , 5 == "5" )             //true
+// console.log( 5 === (3+2) , "5" === (3+2) )     //false
+// console.log( 5 === 5 , 5 === 5 )               //true
+
+// //concantenar (+)
+
+// console.log (5+"3")// 53
+// console.log (5*"3")//15
+// console.log (5+3)//8
+
+// //pida dos numeros aln usuario y sumelos
+
+// let num1=Number(prompt ('dame un num 1')) //
+// let num2=Number(prompt ('dame un num 2')) //
+// let resultado = num1+num2
+// alert ("el resultado de tu suma es" + resultado)
+
+let (prompt ('dame un valor A')); 
+let valorB=Number(prompt ('dame un valor B')) 
+
+let resultado = num1+num2
+let resultado = num1-num2
+let resultado = num1*num2
+let resultado = num1/num2
+
+alert ("el resultado de tu suma es" + resultado,-resultado,)
+
