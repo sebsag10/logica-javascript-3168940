ee57894d639f86fd7329564539e4ed264eae1283
Sebas <sebsag10@gmail.com>
2025-08-04 23:04:30 -0500

    condicionales

---
+++ 3_condicionales/app.js
@@ -0,0 +1,13 @@
+let carro = 30000000
+let dinero_suficiente = 30000000
+let dinero_insuficiente = 29000000
+
+// Condición (si tengo mas de 30 millones puedo compprar el carro)
+if (dinero_suficiente>= carro) {
+console.log("¡puedo comprar el carro!")
+if (dinero_insuficiente<carro)
+    console.log("no lo puedo comprar");
+}
+
+
+
